b5e6d2df5b4a875ac5b2093a94fd64320d5f4a7c
anne-other <anne.c.ince@gmail.com>
2018-07-21 15:53:50 +0100

    adding check to see if customer has sufficient funds before selling pet to customer

--- pet_shop.rb
+++ pet_shop.rb
@@ -70,7 +70,7 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  if pet != nil
+  if pet != nil && customer_can_afford_pet(customer, pet)
     add_pet_to_customer(customer, pet)
     increase_pets_sold(pet_shop, customer_pet_count(customer))
     remove_customer_cash(customer, pet[:price])
